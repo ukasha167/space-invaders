063c1f73a539eb8c687a84fd71116a00fdb1190a
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-04-26 10:58:39 +0500

    UPDATE: Added support for Icon in Title Bar.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,20 @@
+#include "defines.h"
 #include "raylib.h"
 
 int main() {
-    InitWindow(1000, 1000, "Hello, World");
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_NAME);
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(BLACK);
+  Image icon = LoadImage("../assets/images/icon.png");
+  ImageFormat(&icon, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
 
-        EndDrawing();
-    }
-    CloseWindow();
+  SetWindowIcon(icon);
+  UnloadImage(icon);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    EndDrawing();
+  }
+  CloseWindow();
 }
