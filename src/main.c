0a6cc573f40abf628b03d603ed4c8935c4ea0ff7
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-04-26 18:47:09 +0500

    UPDATE: Added Ship with controls

--- src/main.c
+++ src/main.c
@@ -1,36 +1,43 @@
-#include "defines.h"
 #include "raylib.h"
+
+#include "defines.h"
+#include "renderer.h"
+#include "solver.h"
+#include "spaceship.h"
 
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_NAME);
+    SetTargetFPS(TARGET_FPS);
 
-    Image icon = LoadImage("../assets/images/icon.png");
+    Image icon = LoadImage("../assets/assets2D/images/icon.png");
     ImageFormat(&icon, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
 
     SetWindowIcon(icon); // Supported only on Linux & Windows.
                          // (No MacOS. It doesn't have icons in title bar,
-                         // So this line is ignored)
+                         // So this line is ignored by the GLFW)
     UnloadImage(icon);
 
-    Camera3D camera = {0};
-    camera.position = (Vector3){5, 5, 10};
-    camera.target = (Vector3){0, 0, 0};
-    camera.up = (Vector3){0, 1, 0};
-    camera.fovy = 45.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
+    Camera3D camera = {.up = (Vector3){0.0f, 1.0f, 0.0f},
+                       .fovy = 60.0f,
+                       .projection = CAMERA_PERSPECTIVE,
+                       .position = CAMERA_POSITION};
+
+    initSpaceship();
 
     float dt;
     while (!WindowShouldClose()) {
         dt = GetFrameTime();
+
+        updateSpaceship(dt);
+        updateCamera(&camera, spaceship.pos);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         BeginMode3D(camera);
 
-        DrawGrid(20, 1.0f);
-        DrawCube((Vector3){0, 0.5f, 0}, 1, 1, 1, RED);
-        DrawCubeWires((Vector3){0, 0.5f, 0}, 1, 1, 1, MAROON);
+        DrawGrid(200, 1.0f);
+        renderSpaceship(&spaceship);
 
         EndMode3D();
 
