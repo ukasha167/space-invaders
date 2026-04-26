80820b7a4223314a543d7f9917b3450823cadc9f
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-04-26 16:56:00 +0500

    ADD: Separated the implementation. So there everyone will be using the same memory location, instead of creating a new one

---
+++ src/defines.c
@@ -0,0 +1,10 @@
+#include "defines.h"
+
+// Cinematic Offset: Horizontal (1.5), Vertical (2.0), and Depth (5.0)
+const Vector3 SHOULDER_OFFSET = {-1.5f, 2.0f, 5.5f};
+
+const uint16_t TARGET_FPS = 62;
+const uint16_t SCREEN_WIDTH = 1000;
+const uint16_t SCREEN_HEIGHT = 700;
+
+const char GAME_NAME[15] = "Space Invaders";
