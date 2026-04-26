8a2ff1a7340686662c17d74f49af4d95f3d2d4f1
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-04-26 16:54:22 +0500

    UPDATE: Fixed the linkage error

--- src/defines.h
+++ src/defines.h
@@ -3,23 +3,27 @@
 
 #include <stdint.h>
 
+#include "raylib.h"
+
 // A LITTLE NOTE FOR EVERYONE
 // LIMIT OF 1 BYTE:  uint8_t  = 0 - 255 (INCLUSIVE)
 // LIMIT OF 2 BYTES: uint16_t = 0 - 65,535 (INCLUSIVE)
 // LIMIT OF 4 BYTES: uint32_t = 0 - 4,294,967,295 (INCLUSIVE)
 // LIMIT OF 8 BYTES: uint64_t = 0 - 18,446,744,073,709,551,615 (INCLUSIVE)
 
-enum State : uint8_t {
+typedef enum {
     MENU = 0,
     STARTING,
     PLAYING,
-    ENDING,
-    OVER
-};
+    ENDING, OVER
+} State;
 
-const uint16_t SCREEN_WIDTH = 1000;
-const uint16_t SCREEN_HEIGHT = 700;
-const char GAME_NAME[] = "Space Invaders";
-const uint16_t TARGET_FPS = 62;
+extern const Vector3 SHOULDER_OFFSET;
+
+extern const uint16_t TARGET_FPS;
+extern const uint16_t SCREEN_WIDTH;
+extern const uint16_t SCREEN_HEIGHT;
+
+extern const char GAME_NAME[15];
 
 #endif
