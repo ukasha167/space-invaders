1b8ed523896114f75c69666397a0463feca3ff0e
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-04-27 20:26:11 +0500

    UPDATE: Separate files for which only handle the core logic

--- src/solver.c
+++ src/solver.c
@@ -1,8 +1,35 @@
 #include "solver.h"
 
-void updateCamera(Camera3D* camera, Vector3 targetPos) {
+Camera3D camera;
+
+void initSolver() {
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+    camera.fovy = 60.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+    camera.position = CAMERA_POSITION;
+
+    initLazer();
+    initSpaceship();
+}
+
+void updateCamera(Camera3D *camera, Vector3 targetPos) {
     camera->position.z = targetPos.z + CAMERA_GAP;
     camera->target.z = targetPos.z - 5;
+}
+
+void updateGame(const float dt) {
+    if (IsKeyPressed(KEY_SPACE)) {
+        spawnLazer(spaceship.pos, spaceship.speed);
+    }
+
+    updateLazer(dt);
+    updateSpaceship(dt);
+    updateCamera(&camera, spaceship.pos);
+}
+
+void destroySolver() {
+    destroyShip();
+    destroyLazer();
 }
 
 /*
@@ -14,9 +41,9 @@ void updateCamera(Camera3D* camera, Vector3 targetPos) {
          targetPos.z + SHOULDER_OFFSET.z
      };
 
-     float smoothness = 0.1f; // 0.1f is the smoothness. Lower = more "lag", Higher = tighter.
-     cam->position.x += (desiredPos.x - cam->position.x) * smoothness;
-     cam->position.y += (desiredPos.y - cam->position.y) * smoothness;
+     float smoothness = 0.1f; // 0.1f is the smoothness. Lower = more "lag",
+ Higher = tighter. cam->position.x += (desiredPos.x - cam->position.x) *
+ smoothness; cam->position.y += (desiredPos.y - cam->position.y) * smoothness;
      cam->position.z += (desiredPos.z - cam->position.z) * smoothness;
 
      cam->target = targetPos;
--- src/solver.h
+++ src/solver.h
@@ -2,10 +2,17 @@
 #define SOLVER_H
 
 #include "raylib.h"
+
 #include "defines.h"
+#include "lazer.h"
+#include "spaceship.h"
 
 extern const Vector3 CAMERA_POSITION;
+extern Camera3D camera;
 
+void initSolver();
 void updateCamera(Camera3D *camera, Vector3 targetPos);
+void updateGame(const float dt);
+void destroySolver();
 
 #endif
