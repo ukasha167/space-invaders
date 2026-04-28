e33858a4273d5b59484309a44bee93b5529fb4e2
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-04-28 18:58:28 +0500

    UPDATE: Added viewable ranges for X & Z Plane

--- src/defines.c
+++ src/defines.c
@@ -1,6 +1,5 @@
 #include "defines.h"
 
-// Cinematic Offset: Horizontal (1.5), Vertical (2.0), and Depth (5.0)
 const float CAMERA_GAP = 6.0f;
 const Vector3 CAMERA_POSITION = (Vector3){-4.0f, 7.0f, CAMERA_GAP};
 
@@ -11,5 +10,11 @@ const uint16_t SCREEN_HEIGHT = 990;
 const float SHIP_RADIUS = 0.8f;
 const float LAZER_RADIUS = 0.2f;
 const float METEOR_RADIUS = 1.8f;
+
+// DEPENDENT ON THE CAMERA SETTINGS (DO NOT CHANGE)
+const float MIN_VIEWABLE_X_INDEX = -6.5f; // From Origin
+const float MAX_VIEWABLE_X_INDEX = 6.5f; // From Origin
+const float MIN_VIEWABLE_Z_INDEX = 100.0f; // In-Front of the Spaceship's Position
+const float MAX_VIEWABLE_Z_INDEX = 4.0f; // Behind the Spaceship's Position
 
 const char GAME_NAME[] = "Space Invaders";
