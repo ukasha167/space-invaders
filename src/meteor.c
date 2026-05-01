635fdd9645113bc9920d8feccf358d867fc7b360
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-05-01 12:44:54 +0500

    UPDATE: Fixed the spawning parameters & added different textures for the meteors

--- src/meteor.c
+++ src/meteor.c
@@ -1,7 +1,8 @@
 #include "meteor.h"
 
-static Model meteorModel;
-Texture2D meteorTexture;
+static Model meteorModels[MAX_METEORS_TEXTURES];
+static Texture2D meteorTextures[MAX_METEORS_TEXTURES];
+
 Meteor meteors[MAX_METEORS_COUNT];
 
 void initMeteor() {
@@ -12,20 +13,33 @@ void initMeteor() {
 
 void spawnMeteor(const int idx) {
     meteors[idx].pos = (Vector3){
-        (float)GetRandomValue(MIN_VIEWABLE_X_INDEX, MAX_VIEWABLE_X_INDEX),
-        0.0f, (float)GetRandomValue(spaceship.pos.z - MIN_VIEWABLE_Z_INDEX, spaceship.pos.z - MIN_VIEWABLE_Z_INDEX / 2 )};
+        (float)GetRandomValue(MIN_VIEWABLE_X_INDEX, MAX_VIEWABLE_X_INDEX), 0.0f,
+        (float)GetRandomValue(spaceship.pos.z - MIN_VIEWABLE_Z_INDEX / 1.5,
+                              spaceship.pos.z - MIN_VIEWABLE_Z_INDEX)};
 
-    meteors[idx].speed = (float)GetRandomValue(5, 15);
+    meteors[idx].speed = (float)GetRandomValue(15, 20);
     meteors[idx].scale = (Vector3){1.0f, 1.0f, 1.0f};
     meteors[idx].isActive = true;
 }
 
 void loadMeteorModel() {
     Mesh mesh = GenMeshSphere(2.0f, 8, 8);
-    meteorModel = LoadModelFromMesh(mesh);
 
-    meteorTexture = LoadTexture("../assets/assets3D/textures/red.png");
-    meteorModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = meteorTexture;
+    const char TEXTURES[MAX_METEORS_TEXTURES][15] = {"red.png",    "green.png",
+                                                     "light.png",  "orange.png",
+                                                     "purple.png", "dark.png"};
+    char path[45];
+
+    for (int i = 0; i < MAX_METEORS_TEXTURES; i++) {
+        meteorModels[i] = LoadModelFromMesh(mesh);
+
+        const char *path =
+            TextFormat("../assets/assets3D/textures/%s", TEXTURES[i]);
+        meteorTextures[i] = LoadTexture(path);
+
+        meteorModels[i].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture =
+            meteorTextures[i];
+    }
 }
 
 void updateMeteor(const float dt) {
@@ -46,12 +60,18 @@ void drawMeteor() {
             continue;
         }
 
-        DrawModelEx(meteorModel, meteors[i].pos, (Vector3){0, 1, 0}, 0.0f,
-                    meteors[i].scale, WHITE);
+        DrawModelEx(meteorModels[i % MAX_METEORS_TEXTURES], meteors[i].pos,
+                    (Vector3){0, 1, 0}, 0.0f, meteors[i].scale, WHITE);
     }
 }
 
 void freeMeteor() {
-    UnloadTexture(meteorTexture);
-    UnloadModel(meteorModel);
+    UnloadTexture(meteorTextures[0]);
+    UnloadModel(meteorModels[0]);
+
+    for (int i = 1; i < MAX_METEORS_TEXTURES; i++) {
+        meteorModels[i].meshCount = 0;
+        UnloadTexture(meteorTextures[i]);
+        UnloadModel(meteorModels[i]);
+    }
 }
