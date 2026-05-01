9b714b536998cd8684f556850f8208e54b4a9452
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-05-01 20:35:33 +0500

    UPDATE: Shifted the radius calculations to the .h file

--- src/defines.c
+++ src/defines.c
@@ -3,13 +3,9 @@
 const float CAMERA_GAP = 6.0f;
 const Vector3 CAMERA_POSITION = (Vector3){-4.0f, 7.0f, CAMERA_GAP};
 
-const uint16_t TARGET_FPS = 61;
+const uint16_t TARGET_FPS = 62;
 const uint16_t SCREEN_WIDTH = 1660;
 const uint16_t SCREEN_HEIGHT = 990;
-
-const float SHIP_RADIUS = 0.8f;
-const float LAZER_RADIUS = 0.2f;
-const float METEOR_RADIUS = 1.8f;
 
 // DEPENDENT ON THE CAMERA SETTINGS (DO NOT CHANGE)
 const float MIN_VIEWABLE_X_INDEX = -6.5f; // From Origin
